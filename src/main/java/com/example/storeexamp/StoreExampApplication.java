1e4fae9a620f6a5488f85bfb917cc08b02df9126
Alex <2640178@gmail.com>
2021-06-09 07:24:05 +0300

    Initial commit

---
+++ src/main/java/com/example/storeexamp/StoreExampApplication.java
@@ -0,0 +1,13 @@
+package com.example.storeexamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreExampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreExampApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/storeexamp/StoreExampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storeexamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreExampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
